e5aa1b7bf7a452e1483bf882e0a7d6cca5a7b592
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-31 23:02:52 +0200

    tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt install nginx -y
 sudo ufw allow 'Nginx HTTP'
 sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "Hello from the server" > /data/web_static/releases/test/index.html
+echo "Hello from the server" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '52 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;'  /etc/nginx/sites-available/default
