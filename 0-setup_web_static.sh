959705a548807e865268c77a666539e62778c8ff
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-31 23:26:05 +0200

    tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,6 @@ sudo mkdir -p /data/web_static/releases/test/
 echo "Hello from the server" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '52 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;'  /etc/nginx/sites-available/default
+sudo sed -i '52c \ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;' /etc/nginx/sites-available/default
 sudo nginx -t
 sudo service nginx restart
