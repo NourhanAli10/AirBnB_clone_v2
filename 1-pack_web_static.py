d65333577d7556576c3a38a8771593701559aca3
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-01 00:12:57 +0200

    tasks

---
+++ 1-pack_web_static.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""this module generates a .tgz archive from the contents of the web_static folder of
+your AirBnB Clone repo, using the function do_pack."""
+
+def do_pack():
+"""function """
