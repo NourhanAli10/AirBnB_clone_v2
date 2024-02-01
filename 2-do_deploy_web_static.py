356acce6ddfd7c8140061897b5a71786cf46bce8
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-02 01:28:02 +0200

    tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers using the function do_deploy.
 """
+
 from fabric.api import env, put, run
 from os.path import exists
-from datetime import datetime
 
 env.hosts = ["54.90.37.130", "54.90.47.215"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Deploys the web_static archive to the web servers."""
+    if not exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Extract the archive to /data/web_static/releases/
+        archive_filename = archive_path.split('/')[-1]
+        folder_name = archive_filename.split('.')[0]
+        path_release = "/data/web_static/releases/"
+
+        run("mkdir -p {}{}".format(path_release, folder_name))
+        run("tar -xzf /tmp/{} -C {}{}".format(
+            archive_filename, path_release, folder_name))
+
+        # Remove the archive from the web server
+        run("rm /tmp/{}".format(archive_filename))
+
+        # Move the contents of the extracted folder
+        run("mv {0}{1}/web_static/* {0}{1}/".format(
+            path_release, folder_name))
+
+        # Remove the empty web_static folder
+        run("rm -rf {}{}/web_static".format(path_release, folder_name))
+
+        # Remove the old symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s {0}{1} /data/web_static/current".format(
+            path_release, folder_name))
+
         return True
+
     except Exception as e:
         return False
