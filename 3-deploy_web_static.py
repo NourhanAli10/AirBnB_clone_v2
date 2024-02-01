976c8d6c9c43d379c9e6ade821d20f41dab7ee13
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-02 01:40:03 +0200

    tasks

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 
 from fabric.api import env, local, put, run
 from datetime import datetime
-from os.path import exists
+import os
 
 env.hosts = ["54.90.37.130", "54.90.47.215"]
 
