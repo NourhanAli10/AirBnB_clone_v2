a00f3abe955e38be286e9baf3b312d6330ad3acd
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-02 02:46:12 +0200

    tasks

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -78,16 +78,16 @@ def deploy():
 
 def do_clean(number=0):
     """Deletes out-of-date archives"""
-    number = int(number)
-    if number < 0:
-        return
-
-    try:
-        # Delete unnecessary archives in versions folder
-        local("ls -1t versions/ | tail -n +{} | xargs -I {{}} rm versions/{{}}".format(number + 1))
-
-        # Delete unnecessary archives in web_static/releases folder on both servers
-        run("ls -1t /data/web_static/releases/ | tail -n +{} | xargs -I {{}} rm -rf /data/web_static/releases/{{}}".format(number + 1))
-
-    except Exception as e:
-        pass
+    files = local("ls -1t versions", capture=True)
+    file_names = files.split(" ")
+    n = int(number)
+    if n in (0, 1):
+        n = 1
+    for i in file_names[n:]:
+        local("rm versions/{}".format(i))
+    dir_server = run("ls -1t /data/web_static/releases")
+    dir_server_names = dir_server.split(" ")
+    for i in dir_server_names[n:]:
+        if i is 'test':
+            continue
+        run("rm -rf /data/web_static/releases/{}".format(i))
