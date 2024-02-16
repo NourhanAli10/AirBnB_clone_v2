c83a89ee9ea2282a7bfacc50bb42296f7214e462
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-16 19:58:37 +0200

    tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,13 +5,20 @@ this module starts a Flask web application
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """function to print hello HBNB when following the url /"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def print_hbnb():
+    """function to print HBNB when following the url /hbnb"""
+    return "HBNB"
 
 
 if __name__ == '__main__':
