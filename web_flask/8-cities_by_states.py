324a4d4027c91467337372f56d01f9a42c4f183c
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-17 20:14:35 +0200

    tasks

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application
+"""
+
+from flask import Flask, render_template
+import sys
+sys.path.append('../')
+from models import storage
+from models import *
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display a page with sorted states"""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', sorted_states=sorted_states)
+
+
+@app.teardown_appcontext
+def remove_session(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
