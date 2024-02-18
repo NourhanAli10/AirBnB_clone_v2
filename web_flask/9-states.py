7bb14294da1af1e1d66a5b4daab558598a91a7d5
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-18 22:44:19 +0200

    tasks

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,17 +10,23 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def show_states(id=None):
-    """show states"""
-    states = storage.all(State)
-    try:
-        state = states['State.' + id] if id is not None else None
-    except (KeyError):
-        return render_template('9-states.html', states=None, state=None)
-    return render_template('9-states.html',
-                           states=states.values(), state=state)
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
