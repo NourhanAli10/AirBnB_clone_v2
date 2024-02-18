04fcc02c20c6deab79e984be31f75ab110793afd
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-18 19:53:48 +0200

    tasks

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os
+from os import getenv
 
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
+from models import *
 
-storage = DBStorage() if os.getenv(
-    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage() 
+else:
+    storage = FileStorage()
 """A unique FileStorage/DBStorage instance for all models.
 """
 storage.reload()
--- models/city.py
+++ models/city.py
@@ -10,7 +10,6 @@ from models.base_model import BaseModel, Base
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    id = Column(String(60), primary_key=True, nullable=False)
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
