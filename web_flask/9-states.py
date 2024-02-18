e96ef23cf05fc56560fc4b017c2fdcbade41bea9
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-18 22:48:28 +0200

    tasks

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""Starts a Flask web application.
 
-from flask import Flask, render_template
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
-import models
-from models.state import State
-from models.city import City
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -30,10 +33,10 @@ def states_id(id):
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """Remove the current SQLAlchemy Session after each request."""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
